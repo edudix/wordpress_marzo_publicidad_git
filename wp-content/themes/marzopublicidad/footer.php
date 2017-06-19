f60dd15a847348044ecbb4d05893df61f96a93dc
Eduardo Orusco <Eorusco@ZED.COM>
2017-06-19 23:16:49 +0200

    Added changes to avoid the image static loading

--- wp-content/themes/marzopublicidad/footer.php
+++ wp-content/themes/marzopublicidad/footer.php
@@ -8,6 +8,15 @@
 <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
 <script src="<?php echo get_template_directory_uri() . '/assets/js/main.min.js' ?>"></script>
+<script>
+    function init() {
+    var imgDefer = document.getElementsByTagName('img');
+    for (var i=0; i<imgDefer.length; i++) {
+    if(imgDefer[i].getAttribute('data-src')) {
+    imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+    } } }
+    window.onload = init;
+</script>
     <?php wp_footer(); ?>
 </body>
 </html>
