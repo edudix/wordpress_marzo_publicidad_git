cfdac87d2972718976244b0754d18fa02a2f3fcd
Edu <eorusco@marzomarketing.com>
2017-06-22 18:26:51 +0200

    Added GOogle Anlytics tag at header.php for web analytics

--- wp-content/themes/marzopublicidad/header.php
+++ wp-content/themes/marzopublicidad/header.php
@@ -26,6 +26,16 @@
 	<meta name="twitter:image" content="">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/style.css">
 	<?php wp_head(); ?>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-85450073-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </head>
 <body <?php body_class(); ?>>
 <div class="separator"></div>
