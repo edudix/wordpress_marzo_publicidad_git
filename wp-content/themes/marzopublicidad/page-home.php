d85039ede3757f12e1d1e454a7582b12c85b2502
Edu <eorusco@marzomarketing.com>
2017-07-27 14:26:34 +0200

    We have commented the part refered to proyects to keep it hide

--- wp-content/themes/marzopublicidad/page-home.php
+++ wp-content/themes/marzopublicidad/page-home.php
@@ -5,7 +5,7 @@
     <?php get_template_part( 'partials/service_list' ); ?>
     <?php get_template_part( 'partials/city' ); ?>
     <?php get_template_part( 'partials/nuestras_marcas' ); ?>
-    <?php get_template_part( 'partials/ultimos_proyectos' ); ?>
+    <?php /*get_template_part( 'partials/ultimos_proyectos' );*/ ?>
     <?php get_template_part( 'partials/home_blog_section' ); ?>
     <?php get_template_part( 'partials/trabajamos_para_ti' ); ?>
     <?php get_template_part( 'partials/colaboradores' ); ?>
